d16074cc4d36b78acecdbd96cef7eac10c482d9b
Leotrim Halimi <leootrim221@gmail.com>
2025-04-21 21:51:48 +0200

    Implemnt authorization from auth0

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from './users/users.module';
 import { EquipmentsModule } from './equipments/equipments.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -14,7 +15,8 @@ import { MongooseModule } from '@nestjs/mongoose';
       envFilePath: '.env',
       isGlobal: true
     }),
-    MongooseModule.forRoot(process.env.MONGO_URI!)
+    MongooseModule.forRoot(process.env.MONGO_URI!),
+    AuthModule
     ],
   controllers: [AppController],
   providers: [AppService],
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
+    @UseGuards(AuthGuard('jwt'))
     @Get()
     findAll() {
         return this.usersService.findAll()
