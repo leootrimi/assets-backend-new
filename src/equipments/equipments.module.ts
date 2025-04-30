982578b0e9857d02109f7b3fca72bdb89771267b
Leotrim Halimi <leootrim221@gmail.com>
2025-04-30 10:53:51 +0200

    create equipments schema

--- src/equipments/equipments.module.ts
+++ src/equipments/equipments.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { EquipmentsController } from './equipments.controller';
 import { EquipmentsService } from './equipments.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Equipments, EquipmentsSchema } from './schema/equipments.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([
+    { name: Equipments.name, schema: EquipmentsSchema}
+  ])],
   controllers: [EquipmentsController],
   providers: [EquipmentsService]
 })
