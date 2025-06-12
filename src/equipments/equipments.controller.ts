f1e86701243badb04eadc75f644407dd50423c17
Leotrim Halimi <leootrim221@gmail.com>
2025-06-12 09:40:15 +0200

    add microservice

--- src/equipments/equipments.controller.ts
+++ src/equipments/equipments.controller.ts
@@ -1,32 +1,40 @@
-import { Controller, Get, Post, Body, UseGuards, Param, NotFoundException, Put, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Param, NotFoundException, Put, Req, Inject } from '@nestjs/common';
 import { EquipmentsService } from './equipments.service';
 import { EquipmentsDto } from './dto/equipments.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { AssignedToDto } from './dto/assignedTo.dto';
 import { log } from 'console';
+import { ClientProxy, MessagePattern } from '@nestjs/microservices';
 
 @Controller('equipments')
 export class EquipmentsController {
-    constructor(private readonly equipmentService: EquipmentsService) {}
+    constructor(
+        private readonly equipmentService: EquipmentsService,
+        @Inject('EQUIPMENTS_SERVICE') private readonly equipmentClient: ClientProxy
+    ) {}
 
     @Post()
     create(@Body() equipment: EquipmentsDto, @Req() req: any) {
         return this.equipmentService.create(equipment, req.user);
     }
 
-    @UseGuards(AuthGuard('jwt'))
+    // @UseGuards(AuthGuard('jwt'))
+    @MessagePattern()
     @Get()
-    findAll() {
-        return this.equipmentService.findAll()
+    async findAll() {
+        const response = await this.equipmentClient.send('find_equipments', {})
+        return response
     }
 
     @Get('/:id')
-    findById(@Param('id') id: string) {
-        const equipment = this.equipmentService.findById(id)
-        if (!equipment) {
+    async findById(@Param('id') id: string) {
+        // const equipment = this.equipmentService.findById(id)
+        const response = await this.equipmentClient.send('find_by_id', id)
+        
+        if (!response) {
             throw new NotFoundException('Equipment not found');
         }
-        return equipment
+        return response
     }
 
     @Put('/:id')
--- src/equipments/equipments.module.ts
+++ src/equipments/equipments.module.ts
@@ -3,11 +3,19 @@ import { EquipmentsController } from './equipments.controller';
 import { EquipmentsService } from './equipments.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Equipments, EquipmentsSchema } from './schema/equipments.schema';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [MongooseModule.forFeature([
     { name: Equipments.name, schema: EquipmentsSchema}
-  ])],
+  ]),
+  ClientsModule.register([{
+        name: 'EQUIPMENTS_SERVICE',
+        transport: Transport.REDIS,
+        options: { host: 'redis', port: 6379 },
+      
+  }])
+],
   controllers: [EquipmentsController],
   providers: [EquipmentsService]
 })
